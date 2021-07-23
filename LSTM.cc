662e08d69968e29ff46b11af4151bc67a5f81260
david <david@goldbell.org>
2021-07-23 15:40:52 -0700

    moved LSTM.h,cc to LSTM.h,cc.old. moved LSTM_1.h,cc to LSTM.h,cc. modified some comments.  The backprop code has not been changed. And since I will now be using the Adam algorithm for gradient descent, the parameters will be modified.

--- LSTM.cc
+++ LSTM.cc
@@ -4,191 +4,149 @@
 #include "LSTM.h"
 using namespace std;
 
-ColVector<double> squash(ColVector<double> x){
-  for(int i = 0;i < x.nrows();i++)x[i] = sigma(x[i]);
-  return x;
-}
-
-
-void G_c(ColVector<double>& x, ColVector<double>& g){
-  assert(g.nrows() == x.nrows());
-  int dim = x.nrows();
-  for(int i = 0; i < dim;i++) g[i] = sigma(x[i]);
-}
-void dG_c(ColVector<double>& x, ColVector<double>& g){
-  assert(g.nrows() == x.nrows());
-  int dim = x.nrows();
-  for(int i = 0; i < dim;i++)g[i] = sigma(x[i])*sigma(-x[i]);
-}
-void G_d(ColVector<double>& x, ColVector<double>& g){
-  assert(g.nrows() == x.nrows());
-  int dim = x.nrows();
-  for(int i = 0;i < dim;i++) g[i] = 2*sigma(x[i])-1;
-}
-void dG_d(ColVector<double>& x, ColVector<double>& g){
-  assert(g.nrows() == x.nrows());
-  int dim = x.nrows();
-  for(int i = 0;i < dim;i++)g[i] = 2*sigma(x[i])*sigma(-x[i]);
-}
-
 //static variables defined at global scope
 
 int LSTMcell::n_s; // dimension of the state and output vectors
 int LSTMcell::n_x; // dimension of the input vector
-
-  
-/* backward pass temporaries
- * they are esssentially scratch computations that don't need to be saved
- * so they are static to minimize dynamic memory thrashing. Each instance
- * overwrites whatever is there from the last time step.
- *
- */
-ColVector<double> LSTMcell::alpha_du; 
-ColVector<double> LSTMcell::alpha_cu;
-ColVector<double> LSTMcell::alpha_cs;
-ColVector<double> LSTMcell::alpha_cr;
-
-ColVector<double> LSTMcell::d_cu; // G_c'(a_cu)
-ColVector<double> LSTMcell::d_cs; // G_c'(a_cs)
-ColVector<double> LSTMcell::d_cr; // G_c'(a_cr)
-ColVector<double> LSTMcell::d_s; // G_d'(state)
-ColVector<double> LSTMcell::d_du; // G_d'(a_du)
-
-ColVector<double> LSTMcell::chi; // dE/dv
-ColVector<double> LSTMcell::rho; // dE/dr
-//ColVector<double> LSTMcell::gamma; // dE/dg
-ColVector<double> LSTMcell::psi; // dE/ds
-
-/* these are the actual gradients we will use to update parameters for
- * the next iteration.  Each cell object adds its contribution.
- */
-Matrix<double> LSTMcell::dE_dW_xdu;
-Matrix<double> LSTMcell::dE_dW_vdu;
-Matrix<double> LSTMcell::dE_dW_vcu;
-Matrix<double> LSTMcell::dE_dW_xcu;
-Matrix<double> LSTMcell::dE_dW_scu;
-Matrix<double> LSTMcell::dE_dW_vcs;
-Matrix<double> LSTMcell::dE_dW_xcs;
-Matrix<double> LSTMcell::dE_dW_scs;
-Matrix<double> LSTMcell::dE_dW_xcr;
-Matrix<double> LSTMcell::dE_dW_scr;
-Matrix<double> LSTMcell::dE_dW_vcr;
+ColVector<double> LSTMcell::zero;
+RowVector<double> LSTMcell::d_dg;
 
 void LSTMcell::static_initializer(int ss, int xx){
-  
   n_s = ss; // state dimension
   n_x = xx; // input dimension
-
-  //initialize gradients
-  dE_dW_xdu.reset(n_s,n_x); 
-  dE_dW_xcu.reset(n_s,n_x);
-  dE_dW_xcs.reset(n_s,n_x);
-  dE_dW_xcr.reset(n_s,n_x);
-  dE_dW_vcr.reset(n_s,n_s);
-  dE_dW_scr.reset(n_s,n_s);
-  dE_dW_vdu.reset(n_s,n_s);
-  dE_dW_vcu.reset(n_s,n_s);
-  dE_dW_vcs.reset(n_s,n_s);
-  dE_dW_scu.reset(n_s,n_s);
-  dE_dW_scs.reset(n_s,n_s);
-
-  // initialize backward pass temporaries
-  alpha_du.reset(n_s);alpha_cu.reset(n_s);alpha_cs.reset(n_s);alpha_cr.reset(n_s);d_cu.reset(n_s);
-  d_cs.reset(n_s);d_cr.reset(n_s);d_s.reset(n_s);d_du.reset(n_s);chi.reset(n_s);rho.reset(n_s);psi.reset(n_s); 
+  d_dg.reset(n_s);
+  zero.reset(n_s);
+  for(int i = 0;i < n_s;i++)zero[i] = 0;
 }
 
-void LSTMcell::reset(LSTMcell* pc, LSTMcell* nc, Array<Matrix<double>>& p)
-{
-  W_xdu = p[0];  //NOTE:  these are shallow copies.  There's only one actual set of parameters
-  W_xcu = p[1];
-  W_xcs = p[2];
-  W_xcr = p[3];
-  W_vcr = p[4];
-  W_scr = p[5];
-  W_vdu = p[6];
-  W_vcu = p[7];
-  W_vcs = p[8];
-  W_scu = p[9];
-  W_scs = p[10];
+double LSTMcell::sigma(double x){return 1/(1+exp(-x));}
 
-  prev_cell = pc; next_cell = nc;_state.reset(n_s);_readout.reset(n_s),_input.reset(n_x);
-  u.reset(n_s);r.reset(n_s);a_du.reset(n_s);a_cu.reset(n_s);a_cs.reset(n_s);a_cr.reset(n_s);
-  g_cu.reset(n_s);g_cs.reset(n_s);g_cr.reset(n_s);f_chi.reset(n_s);f_psi.reset(n_s);
+ColVector<double> LSTMcell::squash(ColVector<double> x){
+  for(int i = 0;i < x.nrows();i++)x[i] = sigma(x[i]);
+  return x;
 }
 
+ColVector<double> LSTMcell::bulge(ColVector<double> x){
+  for(int i = 0;i < x.nrows();i++) x[i] =2*x[i]-1;
+  return x;
+}
+
+void Gate::reset(RowVector<Matrix<double>> W0){
+  assert(W0.dim() == 3);
+  W = W0; // shallow copy
+  //  cout <<"Gate reset: W:\n"<<W;
+  for(int i = 0;i < 2;i++)assert(W[i].ncols() == LSTMcell::n_s);
+  assert(W[2].ncols() == LSTMcell::n_x);
+  g.reset(LSTMcell::n_s);
+  dg_dw.reset(3);
+  dg_dW.reset(3);
+  for(int k = 0;k < 3;k++){
+    dg_dw[k].reset(W[k].nrows(),W[k].ncols());
+    dg_dW[k].reset(W[k].nrows(),W[k].ncols());
+  }
+}
+
+ColVector<double> Gate::operator()(RowVector<ColVector<double>>& w){
+  // cout << "W[2]: "<<W[2]<<" x: "<<x<<" W[2]*x: "<<W[2]*x;
+  // cout << "W[1]: "<<W[1]<<" s: "<<s<<" W[1]*s: "<<W[1]*s;
+  // cout << "W[0]: "<<W[0]<<" v: "<<v<<" W[0]*v: "<<W[0]*v;
+  ColVector<double>& v = w[0];
+  ColVector<double>& s = w[1];
+  ColVector<double>& x = w[2];
+
+  g = W[0]*v + W[1]*s + W[2]*x;
+  //  cout << "g: "<<g;
+  for(int k = 0;k < W.dim();k++){
+    for(int i = 0;i < g.nrows();i++){
+      double d_sigma_i = LSTMcell::sigma(g[i])*(1-LSTMcell::sigma(g[i]));
+      for(int j = 0;j < W[k].ncols();j++){
+        dg_dw[k](i,j) = d_sigma_i*W[k](i,j);
+        dg_dW[k](i,j) = d_sigma_i*w[k][j];
+      }
+    }
+  }
+  return LSTMcell::squash(g);
+}
+
+
+
+
+void LSTMcell::reset(LSTMcell* pc, LSTMcell* nc, Matrix<Matrix<double>> W0){
+  assert(W0.nrows() == 4 && W0.ncols() == 3);
+  gate.reset(4);
+  RowVector<Matrix<double>> R(3);
+  for(int i = 0;i < 4;i++){
+    R = W0.slice(i,0,1,3);
+    gate[i].reset(R);
+  }
+  w.reset(3);
+  w[0].reset(n_s);
+  w[1].reset(n_s);
+  w[2].reset(n_x);
+  r.reset(n_s);
+  //  input.reset(n_x);
+  
+  prev_cell = pc;
+  next_cell = nc;
+  d_ds.reset(n_s);
+  d_dv.reset(n_s);
+}
+
+void printmat(Matrix<double>& M){cout << M;}
 
 void LSTMcell::forward_step(ColVector<double>& x){
-   _input.copy(x); // save for backward step
-   assert(prev_cell != nullptr);
-   ColVector<double>& v = prev_cell->_readout;
-   ColVector<double>& old_state = prev_cell->_state;
-   a_cu = W_xcu*x + W_scu*old_state + W_vcu*v;
-   a_cs = W_xcs*x + W_scs*old_state + W_vcs*v;
-   a_du = W_xdu*x + W_vdu*v;
-   G_c(a_cu,g_cu);
-   G_c(a_cs,g_cs);
-   G_d(a_du,u); 
-   _state = (old_state&g_cs) + (u&g_cu);
-   a_cr = W_xcr*x + W_scr*_state + W_vcr*v;
-   G_c(a_cr,g_cr);
-   G_d(_state,r);
-   _readout = g_cr&r;
+  w[2].copy(x); // save for backward step
+  assert(prev_cell != nullptr);
+  w[0].copy(prev_cell->w[0]);
+  w[1].copy(prev_cell->w[1]);
+  //cout << "\ngate[0].W:\n"<<gate[0].W;
+  gate[0](w);
+  LSTMcell::bulge(gate[0].g); // convert to tanh(2x);
+  //cout << "\ngate[1].W:\n"<<gate[1].W;
+  gate[1](w);
+  //cout << "\ngate[2].W:\n"<<gate[2].W;
+  gate[2](w);
+  w[1] = (gate[0].g&gate[1].g) + (w[1]&gate[2].g); // update the state
+  //cout << "\ngate[3].W:\n"<<gate[3].W;
+  gate[3](w); 
+  r = bulge(squash(w[1])); // tanh(s/2)
+  w[0] = r&gate[3].g;
+  //cout << "new v: "<<v<<endl;
 }
 
-void LSTMcell::backward_step(ColVector<double>& dE_dv){
-  assert(next_cell != nullptr && prev_cell != nullptr);
-  dG_c(a_cr,d_cr);
-  dG_c(a_cs,d_cs);
-  dG_c(a_cu,d_cu);
-  dG_d(a_du,d_du);
-  dG_c(_state,d_s);
-  chi = dE_dv + next_cell->f_chi;
-  rho = chi&g_cr;
-  alpha_cr = chi&r&d_cr;
-  psi = (rho&d_s) + W_scr*alpha_cr + next_cell->f_psi;
-  alpha_cs = (prev_cell->state())&d_cs&psi;
-  //  alpha_cs = alpha_cs&d_cs;
-  alpha_cu = psi&u&d_cu;
-  alpha_du = psi&g_cu&d_du;
-  f_chi = W_vcu*alpha_cu + W_vcs*alpha_cs + W_vcr*alpha_cr + W_vdu*alpha_du;
-  f_psi = W_scu*alpha_cu + W_scs*alpha_cs + (g_cs&psi);
-
-  RowVector<double> v = prev_cell->_readout.T();
-  RowVector<double> x = _input.T();
-  RowVector<double> s0 = prev_cell->_state.T();
-  RowVector<double> s1 = _state.T();
-  dE_dW_xdu += alpha_du*x;
-  dE_dW_vdu += alpha_du*v;
-  dE_dW_vcu += alpha_cu*v;
-  dE_dW_xcu += alpha_cu*x;
-  dE_dW_scu += alpha_cu*s0;
-  dE_dW_vcs += alpha_cs*v;
-  dE_dW_xcs += alpha_cs*x;
-  dE_dW_scs += alpha_cs*s0;
-  dE_dW_xcr += alpha_cr*x;
-  dE_dW_scr += alpha_cr*s1;
-  dE_dW_vcr += alpha_cr*v;
-}
+// void LSTMcell::backward_step(RowVector<double>& dE_dv){
+//   assert(next_cell != nullptr && prev_cell != nullptr);
+//   d_dv = next_cell->d_dv + dE_dv;  // input from local error and backprop
+//   d_dg = d_dv&r;
   
-LSTM::LSTM(Matrix<double>& d, Matrix<double>& o, Array<Matrix<double>>& p) :
-  data(d),output(o),parameters(p){
+//   for(int k = 0;k < 3;k++){
+//     (d_dg*gate[3].dg_dW[k]
 
+
+
+
+// }
+  
+LSTM::LSTM(Matrix<double>& d, Matrix<double>& o, Matrix<Matrix<double>>& p) :
+  data(d),output(o),parameters(p) {
+  
   LSTMcell::static_initializer(output.nrows(),data.nrows());
-  for(int i = 0;i < 4;i++)assert(data.nrows() == parameters[i].ncols()); // input dimension
-  for(int i = 4;i < 10;i++)assert(output.nrows() == parameters[i].ncols());
-  for(int i = 0;i < 10;i++)assert(output.nrows() == parameters[i].nrows());
-
   int& n_s = LSTMcell::n_s;
+  for(int i = 0;i < 4;i++){
+    assert(data.nrows() == parameters(i,2).ncols() && data.nrows() == LSTMcell::n_x); // input dimension
+    for(int j = 0;j < 2;j++)assert(output.nrows() == parameters(i,j).ncols() && output.nrows() == n_s);
+  }
   ncells = data.ncols(); 
   cells.reset(ncells+2); // first and last cell are initializers
 
   cells[0].reset(nullptr,&cells[1],parameters);
   cells[ncells+1].reset(&cells[ncells],nullptr,parameters);
   for(int i = 1;i <= ncells;i++)cells[i].reset(&cells[i-1],&cells[i+1],parameters);
-  for(int i = 0;i <  n_s;i++){
-    cells[0]._state[i] = cells[0]._readout[i] = 0;
-    cells[ncells+1].f_chi[i] = cells[ncells+1].f_psi[i] = 0;
-  }
+  // initialize first and last cells
+  cells[0].w[0].copy(LSTMcell::zero);
+  cells[0].w[1].copy(LSTMcell::zero);
+  cells[ncells+1].d_ds.copy(LSTMcell::zero);
+  cells[ncells+1].d_dv.copy(LSTMcell::zero);
 }
 
 void LSTM::train(int niters,double pct,double learn,double eps){
--- LSTM.h
+++ LSTM.h
@@ -11,122 +11,78 @@
 
 using namespace std;
 
-inline double sigma(double x){return 1/(1+exp(-x));}
-ColVector<double> squash(ColVector<double> x);
+ 
 
-void G_c(ColVector<double>& x, ColVector<double>& g);
-void dG_c(ColVector<double>& x, ColVector<double>& g);
-void G_d(ColVector<double>& x, ColVector<double>& g);
-void dG_d(ColVector<double>& x, ColVector<double>& g);
+struct Gate {
+  /* A gate is just the function g = sigma(W_v*v + W_s*s + W_x*x). The products are matrix*vector products.
+   * It is called a gate because g is component-wise multiplied by an information signal z = (v,s,or x)
+   * which modifies the amplitude of z.  So a component of g near 1 lets most of the corresponding
+   * component of z pass through, while a value near zero blocks most of that component.
+   
+   * The gate also operates in back-propagation mode.  In this mode, the input is dE/dg where E is some
+   * sort of error signal.  The gate then computes dg/dW_i and dg/dz_i which are then multiplied by dE/dg
+   * to get dE/dW_i for input to the parameter correction logic, and dE/dz_i for further back-propogation
+   * to earlier gates and cells.
+   */
 
+  Matrix<Matrix<double>> W;
+  ColVector<double> g;
+  RowVector<Matrix<double>> dg_dz;
+  RowVector<Matrix<double>> dg_dW;
+ public:
+  Gate(void){}
+  void reset(Matrix<Matrix<double>> W0);
+  ColVector<double> operator()(RowVector<ColVector<double>>& z);
+};
 
 struct LSTMcell {
-  /* There are three gates:
-   * 1. state update: controls a squashed linear combination of the input and the last cell readout
-   *    gate parameters are a squashed linear combination of previous state, input, and last cell readout
-   * 2. state pass-thru: controls the previous state input to the current state accumulator
-   *    gate parameters are a squashed linear combination of previous state, input, and last cell readout
-   * 3. output: controls the cell output which is just the squashed and gated current state
-   *    gate parameters are a squashed linear combination of current state, input and last cell readout
-
+  /* A cell consists of four gates.  cell[0] is special since a) the input vector s is always zero,
+   * and b) the output g is modified to 2*g-1. 
+   * The parameters are 11 matrices arranged in a 4x3 grid W.  W(i,j) 
+   * (i = 0,1,2,3; j = 0,1,2) is the 
+   * matrix which multiplies input vector j(0=v,1=s,2=x) to gate i.  W(0,1) is a dummy matrix.
+   * n_s (resp. n_x) is the dimension of the state (s) and readout (v) vectors (resp. input vector x).  Note that the actual number of state 
+   * (resp. input) variables is n_s-1 (resp. n_x-1) because component 0
+   * of each type is has constant value 1 to provide for bias.   
+   * W(i,2) has dimension n_x by n_s (0<=i<=3).  All other parameter matrices have dimension n_s by n_s.  
+   * The LSTM itself is a linear array of cells. Each one takes a separate input vector x and the readout
+   * vector from the previous cell as the input to gates 0-3. The state output from the previous cell is input
+   * to gates 1 and 2.  All cells use and update the same 11 parameter matrices.
    * There are two outputs:
    * 1. current state, an ungated sum of the gated previous state and the gated non-linear combination of 
         input and last cell output
    * 2. readout, a squashed and gated version of the current state
-
    */
   friend class LSTM;
+  friend class Gate;
   static int n_s; // dimension of the state and output vectors
   static int n_x; // dimension of the input vector
-
-  /* backward pass temporaries
-   * they are esssentially scratch computations that don't need to be saved
-   * so they are static to minimize dynamic memory thrashing. Each instance
-   * overwrites whatever is there from the last time step.
-   *
-   */
-  static ColVector<double> alpha_du; 
-  static ColVector<double> alpha_cu;
-  static ColVector<double> alpha_cs;
-  static ColVector<double> alpha_cr;
-
-  static ColVector<double> d_cu; // G_c'(a_cu)
-  static ColVector<double> d_cs; // G_c'(a_cs)
-  static ColVector<double> d_cr; // G_c'(a_cr)
-  static ColVector<double> d_s; // G_d'(state)
-  static ColVector<double> d_du; // G_d'(a_du)
-
-  static ColVector<double> chi; // dE/dv
-  static ColVector<double> rho; // dE/dr
-  //  static ColVector<double> gamma; // dE/dg
-  static ColVector<double> psi; // dE/ds
-
-  /* these are the actual gradients we will use to update parameters for
-   * the next iteration.  Each cell object adds its contribution.
-   */
-  static Matrix<double> dE_dW_xdu;
-  static Matrix<double> dE_dW_vdu;
-  static Matrix<double> dE_dW_vcu;
-  static Matrix<double> dE_dW_xcu;
-  static Matrix<double> dE_dW_scu;
-  static Matrix<double> dE_dW_vcs;
-  static Matrix<double> dE_dW_xcs;
-  static Matrix<double> dE_dW_scs;
-  static Matrix<double> dE_dW_xcr;
-  static Matrix<double> dE_dW_scr;
-  static Matrix<double> dE_dW_vcr;
-
-  // hidden pointers to the the model parameters. See Matrix.h for details
-  Matrix<double> W_xdu; // from input to a_du 
-  Matrix<double> W_vdu; // from lastcell a_du
-
-  Matrix<double> W_vcu; // from lastcell to a_cu 
-  Matrix<double> W_xcu; // from input a_cu
-  Matrix<double> W_scu; // from state to a_cu
-
-  Matrix<double> W_vcs; // from lastcell to a_cs 
-  Matrix<double> W_xcs; // from input to a_cs
-  Matrix<double> W_scs; // from state to a_cs
-
-  Matrix<double> W_xcr; // from input to a_cr 
-  Matrix<double> W_scr; // from state to a_cr
-  Matrix<double> W_vcr; // from lastcell to a_cr
-
-  /* these parameters are instance specific but are defined here
-   * so that we don't thrash dynamic memory by re-allocating each
-   * iteration.   
-   * they are forward pass temporaries which are used in the backward pass
-   * which is why they are not static
-   */
-  ColVector<double> u; // G_d(input sum)
-  ColVector<double> r; // G_d(state);
-  ColVector<double> a_cu; // (input-gate sum)
-  ColVector<double> a_cs; // (state-gate sum)
-  ColVector<double> a_cr; // (readout-gate sum)
-  ColVector<double> a_du; // (input sum)
-  ColVector<double> g_cu; // G_c(a_cu)
-  ColVector<double> g_cs; // G_c(a_cs)
-  ColVector<double> g_cr; // G_c(a_cr)
-  // outputs
-  ColVector<double> f_chi; // these two are passed to time t-1 in the backward pass (ala BPTT)
-  ColVector<double> f_psi;
-  
-  ColVector<double> _state;
-  ColVector<double> _readout;
+  static ColVector<double> zero;
+  static RowVector<double> d_dg; // temporary storage during backprop
+  Array<Gate> gate;
+  // ColVector<double>& s; // state
+  // ColVector<double>& v; // readout
+  ColVector<double> r; // tanh(s/2)
+  RowVector<ColVector<double>> z;
   LSTMcell* prev_cell;
   LSTMcell* next_cell;
+  // ColVector<double>& input;
+  
+  // Backprop variables: input from next cell, output to prev cell
+  RowVector<double> d_ds; // d(forward error)/d(state)
+  RowVector<double> d_dv; // d(forward error)/d(readout) 
 
-  // input
-  ColVector<double> _input;
 
-public:
   LSTMcell(void) {}
-  void reset(LSTMcell* pc, LSTMcell* nc, Array<Matrix<double>>& p);
-  static void static_initializer(int ss, int dd);
-  ColVector<double> state(void){return _state;}
-  ColVector<double> readout(void){return _readout;}
+  void reset(LSTMcell* pc, LSTMcell* nc, Matrix<Matrix<double>> W0);
   void forward_step(ColVector<double>& x);
-  void backward_step(ColVector<double>& dE_dv);
+  void backward_step(RowVector<double>& dE_dv); // input is d(this_cell error)/d(readout)
+  const ColVector<double>& readout(void){return w[0];}
+
+  static void static_initializer(int ss, int xx);
+  static double sigma(double x);
+  static ColVector<double> squash(ColVector<double> x);
+  static ColVector<double> bulge(ColVector<double> x);
 };
 
 struct LSTM {
@@ -136,14 +92,10 @@ struct LSTM {
   //  int n_x;
   Matrix<double> data; // include an extra row set to 1.0 if you want a bias term
   Matrix<double> output;
-  Array<Matrix<double>> parameters;
+  Matrix<Matrix<double>> parameters; //4x3 grid of parameter matrices
 public:
-  LSTM(Matrix<double>& d, Matrix<double>& o, Array<Matrix<double>>& p);
+  LSTM(Matrix<double>& d, Matrix<double>& o, Matrix<Matrix<double>>& p);
   void train(int max_iters, double pct=1.0, double learn = .1, double eps = 1.0e-8);
-
 };
-    
-
-
 
 #endif
