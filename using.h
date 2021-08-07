68de6d08251a60b73b3a43eb687f5fc7b8a01dc1
david <david@goldbell.org>
2021-08-07 15:00:31 -0700

    re-added using.h

--- using.h
+++ using.h
@@ -1,1 +1,18 @@
-../include/using.h
+#ifndef USING_H
+#define USING_H
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using std::cout;
+using std::cin;
+using std::cerr;
+using std::endl;
+using std::ifstream;
+using std::ofstream;
+using std::istream;
+using std::ostream;
+using std::string;
+using std::stringstream;
+using std::vector;
+#endif
